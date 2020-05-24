9e1f752b0765c21797dfc9a103a9d214314e6ab1
Mateusz <37940021+11609@users.noreply.github.com>
2020-05-24 13:04:41 +0200

    Update trelloagent.py

--- trelloagent.py
+++ trelloagent.py
@@ -1,6 +1,7 @@
 import os
 from trello import TrelloClient
 
+
 # those are required for the connection to work
 # os.environ['TRELLO_API_KEY']
 # os.environ['TRELLO_API_SECRET']
@@ -18,10 +19,32 @@ def demo():
     all_boards = client.list_boards()
     last_board = all_boards[-1]
     last_board.list_lists()
-    my_list = last_board.get_list(list_id)
+    print(last_board.name)
+    # my_list = last_board.get
 
-    for card in my_list.list_cards():
-        print(card.name)
+    # for card in my_list.list_cards():
+    #     print(card.name)
+
+
+def search_boards(target: str = 'TODO'):
+    print("Scanning. Target board: " + target)
+    todo_board, = (boardname for boardname in client.list_boards() if boardname.name == 'TODO')
+    return todo_board
+
+
+def print_board(board):
+    print("-> " + board.name + " " + board.id + " " + board.url)
+    for lst in board.all_lists():
+        print("---> " + lst.name)
+        for card in lst.list_cards():
+            print("-----> " + card.name)
+
+
+def print_all_boards():
+    all_boards = client.list_boards()
+    for board in all_boards:
+        print_board(board)
+
 
 
 def connect():
@@ -40,11 +63,6 @@ def connect():
 def boards():
     return client.list_boards()
 
-
-def scanBoards(target: str = 'TODO'):
-    print("Scanning. Target board: " + target)
-    todo_board, = (boardname for boardname in client.list_boards() if boardname.name=='TODO')
-    return todo_board
 
 client = connect()
 
