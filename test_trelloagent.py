5c2675f910a9932fdad5d4105ff2f6715dc26130
Mateusz <37940021+11609@users.noreply.github.com>
2020-05-26 10:30:44 +0200

    Update test_trelloagent.py

--- test_trelloagent.py
+++ test_trelloagent.py
@@ -1,12 +1,12 @@
-import os
 import unittest
+import setup
 import trelloagent
 
 
 class TestTrelloAgent(unittest.TestCase):
 
     def setUp(self):
-        self.agent = trelloagent.Agent()
+        trelloagent.client = trelloagent.connect()
 
     @classmethod
     def setUpClass(cls):
@@ -19,34 +19,21 @@ class TestTrelloAgent(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-    def test_promptApiKey(self):
-        """user should be prompted to enter api credentials"""
-        pass
-
-    def test_ApiKey(self):
-        self.assertEqual(os.environ['TRELLO_API_KEY'], '<insert correct api key here>');
-        pass
-
     def test_update(self):
-        """test the ability to get update trello board data"""
+        """test the ability to get update on trello board data"""
         pass
 
-    def test_setApiKey(self):
-        """tests functionality of resetting api key"""
-        self.agent.setApiKey('fake_Key')
-        self.assertEqual(self.agent.apiKey, 'fake_key')
-
-
-    def test_getTodoCard(self):
-        """t0d0 card should be obtained from downloaded data"""
+    def test_getTodoBoard(self):
+        """T0D0 board should be obtained"""
+        card = trelloagent.find_board('TODO')
+        self.assertEqual(card.name, 'TODO')
         pass
 
     def test_getTask(self):
         """should be able to retrieve a task"""
-        pass
+        tasks, intensity = trelloagent.tasks()
 
-    def test_rateProductivity(self):
-        """rates productivity based on completed activities"""
+        self.assertTrue(tasks)  # empty list would evaluate to false
         pass
 
 
